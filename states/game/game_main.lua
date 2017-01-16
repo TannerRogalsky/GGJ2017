c970d2096479d825c549f31d661a3d106a9de96b
Tanner Rogalsky <tanner@tannerrogalsky.com>
2017-01-16 10:25:57 -0500

    sight & light

--- states/game/game_main.lua
+++ states/game/game_main.lua
@@ -23,6 +23,41 @@ local function physicsDebugDraw()
   end
 end
 
+local ray_hits = {}
+local function worldRayCastCallback(fixture, x, y, xn, yn, fraction)
+  table.insert(ray_hits, {
+    object = fixture:getUserData(),
+    x = x,
+    y = y,
+    xn = xn,
+    yn = yn,
+    fraction = fraction
+  })
+  return 1
+end
+
+local function rayCast(world, x1, y1, x2, y2, callback)
+  ray_hits = {}
+  world:rayCast(x1, y1, x2, y2, worldRayCastCallback)
+  return ray_hits
+end
+
+local function rayCastClosest(world, x1, y1, x2, y2, callback)
+  rayCast(world, x1, y1, x2, y2, callback)
+  if #ray_hits > 0 then
+    local closest_hit = ray_hits[1]
+    for i,hit in ipairs(ray_hits) do
+      if hit.fraction < closest_hit.fraction then
+        closest_hit = hit
+      end
+    end
+
+    return closest_hit
+  else
+    return nil
+  end
+end
+
 function Main:enteredState()
   local Camera = require("lib/camera")
   self.camera = Camera:new()
@@ -33,7 +68,6 @@ function Main:enteredState()
   local seed = math.floor(love.math.random(math.pow(2, 53)))
   print(string.format("Seed: %u", seed))
   local grid = growingTree(width, height, {random = 1, newest = 1}, seed)
-  -- local grid = growingTree(width, height, {random = 1, newest = 1}, 0)
   grid = symmetricalize(grid, 'both')
   self.map = Map:new(grid)
 
@@ -42,6 +76,8 @@ function Main:enteredState()
   }
 
   self.sprites = require('images.sprites')
+
+  self.mesh = g.newMesh(#self.map.body:getFixtureList() * 4 * 2, 'fan', 'stream')
 
   g.setFont(self.preloaded_fonts["04b03_16"])
 end
@@ -53,6 +89,30 @@ function Main:update(dt)
   end
 end
 
+local function len(x, y)
+  return x * x + y * y
+end
+
+local function getIsLess(a, b)
+  if a[1] >= 0 and b[1] < 0 then return true end
+  if a[1] < 0 and b[1] >= 0 then return false end
+  if a[1] == 0 and b[1] == 0 then
+    if a[2] >= 0 or b[2] >= 0 then return a[2] > b[2] end
+    return b[2] > a[2]
+  end
+
+  local det = a[1] * b[2] - b[1] * a[2]
+  if det < 0 then
+    return true
+  elseif det > 0 then
+    return false
+  end
+
+  local d1 = a[1] * a[1] + a[2] * a[2]
+  local d2 = b[1] * b[1] + b[2] * b[2]
+  return d1 > d2
+end
+
 function Main:draw()
   push:start()
   self.camera:set()
@@ -60,6 +120,37 @@ function Main:draw()
   self.map:draw()
 
   physicsDebugDraw()
+
+  do
+    local MAX_DIST = 200
+    local x1, y1 = self.players[1].attackers[1].x, self.players[1].attackers[1].y
+
+    local body = self.map.body
+    local bx, by = body:getPosition()
+    local hits = {}
+    for _,fixture in ipairs(body:getFixtureList()) do
+      local shape = fixture:getShape()
+      local points = {shape:getPoints()}
+      for i=1,8,2 do
+        local x2, y2 = bx + points[i], by + points[i + 1]
+        -- if len(x1 - x2, y1 - y2) <= MAX_DIST * MAX_DIST then
+          local hit = rayCastClosest(self.world, x1, y1, x2, y2)
+          if hit then
+            table.insert(hits, {hit.x - x1, hit.y - y1})
+          end
+        -- end
+      end
+    end
+
+    table.sort(hits, getIsLess)
+    table.insert(hits, {hits[1][1], hits[1][2]})
+    table.insert(hits, 1, {0, 0})
+    g.setColor(255, 255, 255)
+    self.mesh:setVertices(hits)
+    self.mesh:setDrawRange(1, #hits)
+    g.setWireframe(love.keyboard.isDown('space'))
+    g.draw(self.mesh, x1, y1)
+  end
 
   for i,player in ipairs(self.players) do
     player:draw()
---
+++ states/game/game_points.lua
@@ -0,0 +1,60 @@
+local Points = Game:addState('Points')
+
+local function getIsLess(a, b)
+  if a[1] >= 0 and b[1] < 0 then return true end
+  if a[1] < 0 and b[1] >= 0 then return false end
+  if a[1] == 0 and b[1] == 0 then
+    if a[2] >= 0 or b[2] >= 0 then return a[2] > b[2] end
+    return b[2] > a[2]
+  end
+
+  local det = (a[1]) * (b[2]) - (b[1]) * (a[2])
+  if det < 0 then
+    return true
+  elseif det > 0 then
+    return false
+  end
+
+  local d1 = (a[1]) * (a[1]) + (a[2]) * (a[2])
+  local d2 = (b[1]) * (b[1]) + (b[2]) * (b[2])
+  return d1 > d2
+end
+
+function Points:enteredState()
+  center_x, center_y = 500, 500
+  num_points = 20
+  points = {}
+  love.math.setRandomSeed(0)
+  for i=1,num_points do
+    table.insert(points, {love.math.random(300) - 150, love.math.random(300) - 150})
+  end
+
+  table.sort(points, getIsLess)
+  table.insert(points, 1, {0, 0})
+
+  mesh = g.newMesh(points, 'fan', 'static')
+end
+
+function Points:draw()
+  -- local verts = {}
+  -- for _,v in ipairs(points) do
+  --   table.insert(verts, v[1])
+  --   table.insert(verts, v[2])
+  -- end
+  -- g.setColor(255, 0, 0)
+  -- g.polygon('fill', verts)
+  g.setWireframe(love.keyboard.isDown('space'))
+  g.draw(mesh, center_x, center_y)
+  -- g.setWireframe(false)
+
+  -- g.setColor(255, 255, 255)
+  -- for i,point in ipairs(points) do
+  --   g.li, point[1], point[2])
+  --   g.print(i, point[1], point[2])
+  -- end
+end
+
+function Points:exitedState()
+end
+
+return Points
