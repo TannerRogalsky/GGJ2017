1017cf2e48f9e3cb997020d697cf2f26252ab74c
Tanner Rogalsky <tanner@tannerrogalsky.com>
2017-01-16 14:26:29 -0500

    optimize raycasts

--- light/raycast_closest.lua
+++ light/raycast_closest.lua
@@ -1,27 +1,38 @@
 local ray_hits = {}
-local function worldRayCastCallback(fixture, x, y, xn, yn, fraction)
-  table.insert(ray_hits, {
-    object = fixture:getUserData(),
+local num_hits = 0
+
+-- Private constructor.
+local function new(x, y, fraction)
+  return setmetatable({
     x = x,
     y = y,
-    xn = xn,
-    yn = yn,
     fraction = fraction
   })
+end
+
+-- Do the check to see if JIT is enabled. If so use the optimized FFI structs.
+local status, ffi
+if type(jit) == "table" and jit.status() then
+  status, ffi = pcall(require, "ffi")
+  if status then
+    ffi.cdef "typedef struct { double x, y, fraction; } ray_hit;"
+    new = ffi.typeof("ray_hit")
+  end
+end
+
+local function worldRayCastCallback(fixture, x, y, xn, yn, fraction)
+  num_hits = num_hits + 1
+  ray_hits[num_hits] = new(x, y, fraction)
   return 1
 end
 
-local function rayCast(world, x1, y1, x2, y2, callback)
-  ray_hits = {}
+local function rayCastClosest(world, x1, y1, x2, y2)
+  num_hits = 0
   world:rayCast(x1, y1, x2, y2, worldRayCastCallback)
-  return ray_hits
-end
-
-local function rayCastClosest(world, x1, y1, x2, y2, callback)
-  rayCast(world, x1, y1, x2, y2, callback)
-  if #ray_hits > 0 then
+  if num_hits > 0 then
     local closest_hit = ray_hits[1]
-    for i,hit in ipairs(ray_hits) do
+    for i=1,num_hits do
+      local hit = ray_hits[i]
       if hit.fraction < closest_hit.fraction then
         closest_hit = hit
       end
