1c756da0f14a096aff6df5129b46bc58ce1d1efa
Tanner Rogalsky <tanner@tannerrogalsky.com>
2017-01-20 15:08:55 -0500

    physics callbacks

--- states/game/game_main.lua
+++ states/game/game_main.lua
@@ -40,11 +40,29 @@ local function physicsDebugDraw()
   g.pop()
 end
 
+local physics_callback_names = {"begin_contact", "end_contact", "presolve", "postsolve"}
+local physics_callbacks = {}
+for _,callback_name in ipairs(physics_callback_names) do
+  local function callback(fixture_a, fixture_b, contact, ...)
+    local object_one, object_two = fixture_a:getUserData(), fixture_b:getUserData()
+    -- print(callback_name, object_one, object_two, ...)
+    local nx, ny = contact:getNormal()
+    if object_one and is_func(object_one[callback_name]) then
+      object_one[callback_name](object_one, object_two, contact, nx, ny, ...)
+    end
+    if object_two and is_func(object_two[callback_name]) then
+      object_two[callback_name](object_two, object_one, contact, -nx, -ny, ...)
+    end
+  end
+  table.insert(physics_callbacks, callback)
+end
+
 function Main:enteredState()
   local Camera = require("lib/camera")
   self.camera = Camera:new()
 
   self.world = love.physics.newWorld(0, 0)
+  self.world:setCallbacks(unpack(physics_callbacks))
 
   local width, height = math.floor(16 * 0.4), math.floor(9 * 0.4)
   local seed = math.floor(love.math.random(math.pow(2, 53)))
