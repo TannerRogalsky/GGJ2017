387099c5e0e14a9c4fb8682b31e6404430b34403
Tanner Rogalsky <tanner@tannerrogalsky.com>
2017-01-24 13:15:53 -0500

    raycast fraction tuning

--- light/raycast_closest.lua
+++ light/raycast_closest.lua
@@ -1,32 +1,21 @@
-local ray_hits = {}
-for i=1,1000 do
-  ray_hits[i] = {x = 0, y = 0, fraction = 0}
-end
-local num_hits = 0
+local ray_hit = {x = 0, y = 0}
+local did_hit = false
 
 local function worldRayCastCallback(fixture, x, y, xn, yn, fraction)
-  if fixture:isSensor() then return 1 end
+  if fixture:isSensor() then return -1 end
 
-  num_hits = num_hits + 1
-  ray_hits[num_hits].x = x
-  ray_hits[num_hits].y = y
-  ray_hits[num_hits].fraction = fraction
-  return 1
+  did_hit = true
+  ray_hit.x = x
+  ray_hit.y = y
+
+  return fraction
 end
 
 local function rayCastClosest(world, x1, y1, x2, y2)
-  num_hits = 0
+  did_hit = false
   world:rayCast(x1, y1, x2, y2, worldRayCastCallback)
-  if num_hits > 0 then
-    local closest_hit = ray_hits[1]
-    for i=1,num_hits do
-      local hit = ray_hits[i]
-      if hit.fraction < closest_hit.fraction then
-        closest_hit = hit
-      end
-    end
-
-    return closest_hit
+  if did_hit then
+    return ray_hit
   else
     return nil
   end
